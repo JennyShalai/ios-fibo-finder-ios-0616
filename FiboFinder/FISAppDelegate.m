fb8c5052b19213d156f566a2791090c1126e51c3
JennyShalai <jenny.shalai@gmail.com>
2016-05-13 11:51:48 -0400

    done

--- FiboFinder/FISAppDelegate.m
+++ FiboFinder/FISAppDelegate.m
@@ -9,10 +9,31 @@
     return YES;
 }
 
-/**
- 
- * Write your method implemenation here.
- 
- */
+- (NSArray *)arrayWithFibonacciSequenceToIndex: (NSUInteger)index {
+    
+    NSMutableArray *sequence = [[NSMutableArray alloc] init];
+    
+    for (NSUInteger i = 0; i < index + 1; i++) {
+        if (i == 0) {
+            [sequence addObject:@0];
+        }   else if (i == 1) {
+                [sequence addObject:@1];
+        }   else {
+            
+            NSNumber *valueOfPreviosElement = sequence[i-1];
+            NSNumber *valueOfPreviosOfPreviosElement = sequence[i-2];
+            
+            NSInteger valueOfPreviosElementInt = [valueOfPreviosElement integerValue];
+            NSInteger valueOfPreviosOfPreviosElementInt = [valueOfPreviosOfPreviosElement integerValue];
+            
+            NSNumber *lastNumberInNewArray = @(valueOfPreviosElementInt + valueOfPreviosOfPreviosElementInt);
+            
+            [sequence addObject:lastNumberInNewArray];
+
+        }
+       //  NSLog(@"%@", sequence);
+    }
+    return sequence;
+}
 
 @end
